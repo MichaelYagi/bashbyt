07b86d0e5f39b1c0a53929839fa7021d9ebbe686
Michael Yagi <michaeltyagi@gmail.com>
2024-11-25 07:54:52 -0800

    add filename to .gitignore

--- run_scripts.sh
+++ run_scripts.sh
@@ -13,9 +13,7 @@ function exit_script() {
 zero=0
 set -f
 
-# --------------------
-# db_characters
-
+# Define ttl buckets
 five_s_ttls=5
 twenty_s_ttls=20
 thirty_s_ttls=30
@@ -24,18 +22,14 @@ fifteen_m_ttls=900
 thirty_m_ttls=1800
 one_h_ttls=3600
 
-# Run each script for duration consecutively
+# --------------------
+
 # Pass ttl in seconds
 # Generate webp file
 db_run_cmd="./db_characters/run_script.sh $two_m_ttls"
 
-# --------------------
-# Run each script for duration consecutively
-tech_news_run_cmd="./api_text/tech_news/run_script.sh $two_m_ttls"
-
-# Run all commands before updating on a schecule
-# source $tech_news_run_cmd
-source $db_run_cmd
+# ----------
+# tech_news_run_cmd="./api_text/tech_news/run_script.sh $two_m_ttls"
 
 # --------------------
 # Push to Tydbyt
@@ -44,12 +38,12 @@ source $db_run_cmd
 two_m_loop() {
     while true
     do
-        sleep $two_m_ttls
-
         # source $tech_news_run_cmd
         source $db_run_cmd
 
         echo "2m loop"
+
+        sleep $two_m_ttls
     done
 }
 
@@ -62,10 +56,9 @@ two_m_loop &
 # test_loop() {
 #     while true
 #     do
-#         sleep 2
 #         echo "test"
-
-#         # break
+#         sleep 2
+#         
 #     done
 # }
 
