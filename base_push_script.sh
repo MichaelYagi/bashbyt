f033fd9dd28f850823cd098467b32c6f9ce59db8
Michael Yagi <michaeltyagi@gmail.com>
2024-11-26 18:30:29 -0800

    Update

--- base_push_script.sh
+++ base_push_script.sh
@@ -11,20 +11,21 @@ device_id=$3
 echo "path_to_webp: $path_to_webp"
 echo "installation_id: $installation_id"
 
-# Through API endpoint - doesn't play full animation
 . ./tidbyt.config
 api_key=$api_key
-header_one="Authorization: Bearer $api_key"
-header_two="Content-Type: application/json"
-url="https://api.tidbyt.com/v0/devices/$device_id/push"
-b64=$(cat $path_to_webp | base64)
-b64="${b64//$'\n'/}"
-b64="${b64//$''/}"
-# Ensure data isn't too long for command using file
-echo "{\"installationID\": \"$installation_id\",\"background\":true,\"image\": \"$b64\"}" > /tmp/data.json
-curl_command="curl --header \"$header_one\" --header \"$header_two\" -v \"$url\" -d @/tmp/data.json"
-eval $curl_command
 
-# Through Pixlet command - doesn't play full animation - no ability to run in background
-# pixlet_command="pixlet push --installation-id $installation_id \"$device_id\" $path_to_webp"
-# eval $pixlet_command
+# Through API endpoint - does not play full animation
+# header_one="Authorization: Bearer $api_key"
+# header_two="Content-Type: application/json"
+# url="https://api.tidbyt.com/v0/devices/$device_id/push"
+# b64=$(cat $path_to_webp | base64)
+# b64="${b64//$'\n'/}"
+# b64="${b64//$''/}"
+# # Ensure data isn't too long for command using file
+# echo "{\"installationID\": \"$installation_id\",\"background\":true,\"image\": \"$b64\"}" > /tmp/data.json
+# curl_command="curl --header \"$header_one\" --header \"$header_two\" -v \"$url\" -d @/tmp/data.json"
+# eval $curl_command
+
+# Through Pixlet command - does not play full animation
+pixlet_command="pixlet push --api-token $api_key --background --installation-id $installation_id \"$device_id\" $path_to_webp"
+eval $pixlet_command
